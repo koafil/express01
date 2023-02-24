19469e3b247d786636fcad4ccb6f1f63c0215a70
kfil <koafil@gmail.com>
2023-02-24 18:24:55 +0300

    POST

--- index.js
+++ index.js
@@ -2,9 +2,14 @@ import express from 'express'
 
 const PORT = 5000
 const app = express()
+app.use(express.json())
 app.get('/',(req,res)=>{
   console.log(req.query)
   res.status(200).json('Server is working!')
+})
+app.post('/',(req,res)=>{
+  console.log(req.body)
+  res.status(200).json('Server is working (post)')
 })
 app.listen(PORT,()=>console.log('Server started on port: '+PORT))
 
