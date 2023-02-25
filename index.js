3e705c69cd5ebaa3ba26979f4f6dc43214a266a3
kfil <koafil@gmail.com>
2023-02-25 11:50:19 +0300

    mysql2 pool promise

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express from 'express'
+import mysql from 'mysql2';
+const config = {
+  host: '192.168.50.2',
+  user: 'test1',
+  password: 'testsql',
+  database: 'api'
+};
+const pool = mysql.createPool(config).promise();
 
 const PORT = 5000
 const app = express()
 app.use(express.json())
-app.get('/',(req,res)=>{
-  console.log(req.query)
-  res.status(200).json('Server is working!')
-})
-app.post('/',(req,res)=>{
-  console.log(req.body)
-  res.status(200).json('Server is working (post)')
-})
+app.get('/',async (req,res)=>{
+  try {
+    let result = await pool.execute('SELECT * FROM users');
+    res.status(200).send(result[0]);
+  }
+  catch (e) { res.status(500).send(e);}
+
+});
 app.listen(PORT,()=>console.log('Server started on port: '+PORT))
 
