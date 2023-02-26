7544ebf97178a746b0d86aaa3d938f3d2b2701ef
kfil <koafil@gmail.com>
2023-02-26 16:46:02 +0300

    Декомпозиция router.js

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
 import express from 'express'
-import mysql from 'mysql2';
-const config = {
-  host: '192.168.50.2',
-  user: 'test1',
-  password: 'testsql',
-  database: 'api'
-};
-const pool = mysql.createPool(config).promise();
+import router from "./router.js";
 
 const PORT = 5000
 const app = express()
 app.use(express.json())
-app.get('/',async (req,res)=>{
-  try {
-    let conn = await pool.getConnection();
-    let [rows, fields] = await conn.query('SELECT * FROM tovars');
-    conn.release();
-    res.status(200).send(rows);
-  }
-  catch (e) { res.status(500).send(e);}
-
-});
+app.use('/',router)
 app.listen(PORT,()=>console.log('Server started on port: '+PORT))
 
---
+++ router.js
@@ -0,0 +1,25 @@
+import Router from "express";
+
+import mysql from 'mysql2';
+const config = {
+  host: '192.168.50.2',
+  user: 'test1',
+  password: 'testsql',
+  database: 'api'
+};
+const pool = mysql.createPool(config).promise();
+
+const router = new Router;
+
+router.get('/',async (req,res)=>{
+  try {
+    let conn = await pool.getConnection();
+    let [rows, fields] = await conn.query('SELECT * FROM users');
+    conn.release();
+    res.status(200).send(rows);
+  }
+  catch (e) { res.status(500).send(e);}
+
+});
+
+export default router;
